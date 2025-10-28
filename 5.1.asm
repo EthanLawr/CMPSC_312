79a14056de68e8fddd2775e533fae729a313c4d3
Ethan Lawrence <ethan.h.lawr@gmail.com>
2025-10-27 21:17:59 -0400

    first commit

---
+++ 5.1.asm
@@ -0,0 +1,11 @@
+# Complete 2 tasks as directed by ZyBooks Challenges
+# Task 1: Create Pass DM[$t3] to the YearlySalary subroutine, and store the return value to DM[$t4].
+lw   $t1, 0($t3)        # Load argument from DM[$t3] into $t1
+jal  YearlySalary        # Call the subroutine
+sw   $t2, 0($t4)         # Store return value from $t2 into DM[$t4]
+j    Done                 # Return 
+
+# Task 2: Create a YearlySalary Function
+mul $t4, $t3, $t3 # $t4 = $t3 * $t3
+addi $t4, $t4, 5 # $t4 = $t4 + 5
+j Done # jump to Done
---
+++ README.md
@@ -0,0 +1,3 @@
+# MIPS Assembly Code Review
+
+This is a review of the MIPS Assembly programs I created during my CMPSC 312: Computer Organization and Architecture class. Each file is named by the section of the book it pertains to.
